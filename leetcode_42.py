fc68c23a2257f532e75b7f1188fd81a7ad2b3f59
memoregoing <memoregoing@gmail.com>
2020-10-26 22:11:29 +0900

    Solving a problem leetcode_42

--- leetcode_42.py
+++ leetcode_42.py
@@ -14,6 +14,6 @@ class Solution:
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    Solution.trap()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
