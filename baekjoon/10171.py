2cdbfed0b6930d30079d8f5e131da94ba599c764
memoregoing <memoregoing@gmail.com>
2020-11-06 15:12:32 +0900

    today ps

---
+++ baekjoon/10171.py
@@ -0,0 +1,9 @@
+import sys
+
+def main():
+    sys.stdout.write("\    /\\" + '\n')
+    sys.stdout.write(" )  ( ')" + '\n')
+    sys.stdout.write("(  /  )" + '\n')
+    sys.stdout.write(" \(__)|" + '\n')
+if __name__ == '__main__':
+    main()
--- leetcode_42.py
+++ leetcode_42.py
@@ -3,12 +3,18 @@ from typing import List
 class Solution:
     def trap(self, height: List[int]) -> int:
         stack = []
+        volume = 0
 
         for i in range(len(height)):
             while stack and height[i] > height[stack[-1]]:
                 top = stack.pop()
+
                 if not stack:
                     break
+                distance = i - stack[-1] - 1
+                min_high = min(height[i], height[stack.pop()]) * distance
+                volume += min_high
+            stack.append(i)
 
 
 
